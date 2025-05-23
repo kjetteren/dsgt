f073b7d7165d8e5212d4d828393bb1e5e49e78d6
Ruslan Galiullin <160370317+kjetteren@users.noreply.github.com>
2025-05-23 12:10:42 +0200

    Removed the SOAP and Java RMI files

--- testSOAP.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-HOST="${1:-localhost}"
-PORT="${2:-8080}"
-
-echo "Starting SOAP wrk stress test on $HOST:$PORT"
-
-wrk -t12 -c400 -d120s --latency -s testSOAP.lua http://$HOST:$PORT
-
